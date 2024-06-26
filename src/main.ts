8d8c64d070df3ed4ddd0296c75b99043514e038a
drumath <2237k.drumath@gmail.com>
2024-06-27 03:24:07 +0900

    :sparkles: cittyとconsolaでCLIっぽい挙動を実現

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,31 @@
-const helloLog = () => {
-  console.log("Hello");
-};
+import { defineCommand, runMain as _runMain } from "citty";
+import { consola } from "consola";
 
-export const runMain = helloLog;
+const command = defineCommand({
+  args: {
+    name: {
+      type: "string",
+      alias: "n",
+    },
+  },
+  run: async (context) => {
+    const name =
+      context.args.name == null
+        ? await consola.prompt("What's your name?", { type: "text" })
+        : context.args.name;
+
+    console.log("\n");
+
+    const msg = `Hello, ${name}`;
+    consola.info(msg);
+    consola.success(msg);
+    consola.fail(msg);
+    consola.start(msg);
+    consola.warn(msg);
+    consola.error(msg);
+    consola.silent(msg);
+  },
+});
+
+export const runMain = () => _runMain(command);
+
